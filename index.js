6f36b40a4187901bda853418fcd40439aafa444d
Tiaraju <tiaraju@vtex.com.br>
2017-09-11 18:54:40 -0300

    adding stupid validation func

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 #!/usr/bin/env node
 var request = require('superagent');
+var fs = require("fs");
 
 
 // We could read the request from a given json, make the request, and validate its response on the .end callback
-request.get("http://httpbin.org/ip")
-.end(function(err, res){
-	var ip = res.body.origin;
-	console.log("testing works with ip response: "+ ip);
-	//validateResponse();
+var SPEC_FILE_PATH = "spec/test.json"
+
+var requests = JSON.parse(fs.readFileSync(SPEC_FILE_PATH, 'utf8'));
+
+requests.forEach(function(req){
+	
+	request.get(req.url)
+	
+	.end(function(err, res){
+		var ip = res.body.origin;
+		validateResponse(req.expectedResponse, res.body);
+	});
+
 });
+
+
+var validateResponse = function(expected, response){
+	JSON.stringify(expected) === JSON.stringify(response)? console.log("all tests passed"): console.log("FAIL");
+}
+
+
